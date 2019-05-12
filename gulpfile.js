b313f60262a2e5bb546b438cf2f3ddc951d5a3e7
Davide Mandelli <davide.mandelli@ext.illimity.com>
2019-05-12 18:18:05 +0200

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,54 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
-gulp.task('sass', async function () {
-    gulp.src('styles/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('styles'))
-        .pipe(browserSync.stream())
-});
+function style() {
+    return gulp.src('./styles/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('./styles'))
+        .pipe(browserSync.stream());
 
-gulp.task('browserSync', async function () {
+}
+
+function watch() {
     browserSync.init({
         server: {
             baseDir: './'
-        },
-    })
-});
+        }
+    });
+    gulp.watch('./styles/scss/**/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
 
-gulp.task('watch', gulp.parallel('browserSync', 'sass'), function () {
-    gulp.watch('styles/scss/**/*.scss', gulp.series('sass'));
-});
+exports.style = style;
+exports.watch = watch;
+
+// var config = {
+//     server: {
+//         baseDir: './'
+//     },
+//     files: [
+//         './styles/scss/**/*.scss'
+//     ]
+// };
+
+// gulp.task('sass', async function () {
+//     gulp.src('styles/scss/**/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest('./dist/styles'))
+//         .pipe(browserSync.stream())
+// });
+
+// gulp.task('browserSync', function () {
+//     browserSync.init(config);
+// });
+
+// gulp.task('reload', function (done) {
+//     browserSync.reload();
+//     done();
+// });
+
+// gulp.task('watch', gulp.parallel('browserSync', 'sass'), function () {
+//     gulp.watch('styles/scss/**/*.scss', gulp.series('sass', 'reload'));
+// });
