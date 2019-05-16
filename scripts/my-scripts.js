23898864fd5d063d4a6544a155b5c6fa3c194ea3
Davide Mandelli <davide.mandelli@ext.illimity.com>
2019-05-17 00:20:27 +0200

    Fixed flexbox bugs on IE11, added OwlCarousel2 for header section

--- scripts/my-scripts.js
+++ scripts/my-scripts.js
@@ -204,4 +204,22 @@ $(document).ready(function () {
     loadTabs();
     validateForm();
     cookieAcceptConsent();
+
+    $('.slider-active').owlCarousel({
+        loop: true,
+        autoplay: true,
+        nav: true,
+        navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
 });
